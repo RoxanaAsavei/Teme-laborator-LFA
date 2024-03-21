dd8e044c58b8b6af3df17f3a333a5831b912c223
RoxanaAsavei <146893046+RoxanaAsavei@users.noreply.github.com>
2024-03-21 21:34:46 +0200

    Update Cod.cpp

--- Temă 1/Cerința b)/Cod.cpp
+++ Temă 1/Cerința b)/Cod.cpp
@@ -5,24 +5,31 @@ using namespace std;
 ifstream fin("input.txt");
 ofstream fout("output.txt");
 
-int N;
-map<int, int> stari; // val_nodului - al catalea a fost citit
-int M;
-vector<pair<int, char> > graf[VALMAX + 5];
-vector<string> cuvinte;
+int N; // nr de stari
+map<int, int> stari; // val_nodului - al catalea a fost citit (ii asociez fiecarui nod un indice) ex: 149292 -> 0
+vector<int> indice_stare; // fix continutul din stari, doar ca cheie devine valoare si valoarea devine cheie ex: 0 -> 149292
+int M; // nr de tranzitii
+vector<pair<int, char> > graf[VALMAX + 5]; // modelarea grafului
+// pt fiecare nod o sa tin minte un vector cu elemente de forma (vecin, litera cu care ajung in vecin)
+
+int init_state; // starea initiala
+
+int ct_finale; // nr de stari finale
+bitset<VALMAX+5> finale; // marcam cu 1 starile care sunt finale 
+
 int nr_cuvinte;
-int init_state;
-vector<int> stari_finale;
-int ct_finale;
-queue<pair<int, int> > coada; // (ind_nod, ind_litera_cu_care_am_ajuns_aici)
+vector<string> cuvinte;
+
+queue<pair<int, int> > coada; // (indice nod, indicele ultimei litere din cuvant cu care am ajuns in starea asta)
 
 void Citire() {
     int x, y;
     char litera;
     fin >> N;
-    for(int i = 1; i <= N; ++i) {
+    for(int i = 0; i < N; ++i) {
             fin  >> x;
             stari[x] = i;  // nodului x ii asociez indicele i
+            indice_stare.push_back(x);
         }
     fin >> M;
     for(int i = 1; i <= M; ++i) {
@@ -35,9 +42,9 @@ void Citire() {
     fin >> ct_finale;
     for(int i = 1; i <= ct_finale; ++i) {
         fin >> x;
-        stari_finale.push_back(stari[x]);
+        finale[stari[x]] = 1;
     }
-
+    
     fin >> nr_cuvinte;
     string cuvant;
     for(int i = 1; i <= nr_cuvinte; ++i) {
@@ -53,51 +60,71 @@ void Initializare_coada() {
     }
 }
 
-// ret 1 daca cuvantul apartine, 0 in caz contrar
+void Afisare_drum(int nod, int indice_litera, vector<pair<int, int> > ascendenti[]) {
 
-bool Apartine(string cuvant) {
-    int p =  0, n = cuvant.length();
+    if(indice_litera != -1) { // nu e starea initiala
+        // ma uit sa vad de unde as fi putut ajunge aici
+        int i;
+        for( i = 0; i < ascendenti[nod].size(); ++i) {
+            if(ascendenti[nod][i].second == indice_litera) {
+                break; 
+            }
+        }
+        Afisare_drum(ascendenti[nod][i].first, indice_litera - 1, ascendenti);
+    }
+    fout << indice_stare[nod] << " ";
+}
+
+void Acceptat(string cuvant) { // afiseaza DA daca cuvantul e acceptat + drumul aferent, altfel afiseaza NU
+    vector<pair<int, int> > ascendenti[VALMAX + 5];   
+    /**
+     * Folosim ascendenti doar pentru cerinta suplimentara (afisarea drumului pentru cuvintele acceptate)
+     * Facem un BFS sa vedem daca cuvantul e acceptat sau nu si in timp ce facem asta, pentru fiecare nod x in 
+     * care ajungem vom tine in 
+     * ascendenti[x] perechi de forma (nod din care am ajuns in x, pozitia literei din cuvant care m-a adus in x)
+    */
+    int p =  0, n = cuvant.length(); // p -> indicele literei curente
     Initializare_coada();
     coada.push({stari[init_state], -1}); // pun indicele starii initiale
     while(p < n) {
-        bool ok = 0; // ca sa vad daca pot continua sau n
+        bool ok = 0; // ca sa vad daca pot continua sau nu
         while(!coada.empty() && coada.front().second == p - 1) {// stare pentru care s-a putut obtine prefixul 0..p-1
-            int nod_curent = coada.front().first; // inerc sa vad daca pot continua cu litera de la poz p
+            int nod_curent = coada.front().first; // incerc sa vad daca pot continua cu litera de la poz p
             coada.pop();
             // ma uit sa vad daca pot continua cu ce imi trebuie
             for(int i = 0; i < graf[nod_curent].size(); ++i)
                 if(graf[nod_curent][i].second == cuvant[p]) {
                     coada.push({graf[nod_curent][i].first, p});
+                    ascendenti[graf[nod_curent][i].first].push_back({nod_curent, p});
                     ok = 1;
             }
         }
 
-        if(ok == 1) p++;
-        else return 0;
+        if(ok == 1) p++; // merg mai departe, life's good
+        else { // lipsa de tranzitie
+            fout << "NU\n";
+            return;
+        } 
     }
 
     while(!coada.empty()) {
         int posibil = coada.front().first;
         coada.pop();
-        for(int i = 0; i < stari_finale.size(); ++i) {
-            if(posibil == stari_finale[i])
-                return 1;
+        if(finale[posibil] == 1) {
+            fout << "DA\n";
+            Afisare_drum(posibil, n - 1, ascendenti);
+            fout << "\n";
+            return;
         }
     }
 
-    return 0;
+    fout << "NU\n";
 
 }
 
 void Rezolvare() {
     for(int i = 0; i < cuvinte.size(); ++i) {
-        bool ans = Apartine(cuvinte[i]);
-        if(ans == 1) {
-            fout << "DA" << endl;
-        }
-        else {
-            fout << "NU" << endl;
-        }
+       Acceptat(cuvinte[i]);
     }
 
 }
